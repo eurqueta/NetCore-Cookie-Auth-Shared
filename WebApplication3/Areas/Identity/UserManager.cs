35aa0af9aa4d710d3b35df179c628ccc9bf0a81a
Eduardo Urqueta <eurqueta@gmail.com>
2019-06-05 10:14:57 -0400

    Commit inicial

---
+++ WebApplication3/Areas/Identity/UserManager.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Areas.Identity
+{
+    public class UserConfigurator
+    {
+
+        string _connectionString;
+        public UserConfigurator(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+
+        public async void SignIn(HttpContext httpContext,  bool isPersistent = false)
+        {
+
+
+                ClaimsIdentity identity = new ClaimsIdentity(GetUserClaims() ,CookieAuthenticationDefaults.AuthenticationScheme);
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+            
+        }
+
+        public async Task SignOut(HttpContext httpContext)
+        {
+            await httpContext.SignOutAsync();
+        }
+
+        private IEnumerable<Claim> GetUserClaims()
+        {
+            List<Claim> claims = new List<Claim>();
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, "eurqueta"));
+            claims.Add(new Claim(ClaimTypes.Name, "Eduardo"));
+            claims.Add(new Claim(ClaimTypes.Email, "eduardo@gmail.com"));
+            claims.AddRange(this.GetUserRoleClaims());
+            return claims;
+        }
+
+        private IEnumerable<Claim> GetUserRoleClaims()
+        {
+            List<Claim> claims = new List<Claim>();
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, "eurqueta"));
+            claims.Add(new Claim(ClaimTypes.Role, "rol"));
+            return claims;
+        }
+
+
+    }
+}
---
+++ WebApplication3/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Areas.Identity;
+
+namespace WebApplication3.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserConfigurator _userManager;
+
+        public AccountController(UserConfigurator userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                //authenticate
+
+                _userManager.SignIn(this.HttpContext);
+                return RedirectToAction("Search", "Home", null);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("summary", ex.Message);
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _userManager.SignOut(this.HttpContext);
+
+            return RedirectToAction("Index", "Home", null);
+        }
+    }
+}
